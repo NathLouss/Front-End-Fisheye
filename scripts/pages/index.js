36b3447e5085965105430d00a8f777ace4380f42
nathalie loussouarn <natel@hotmail.com>
2022-06-16 22:17:59 +0200

    import data with method fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,48 +1,62 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    const photographersData = "../../data/photographers.json";
 
-    async function displayData(photographers) {
-        // pick up photographer html section
-        const photographersSection = document.querySelector(".photographer_section");
+    const response = await fetch(photographersData);
+    const data = await response.json();
+    // await fetch(photographersData)
+    //     .then(response => await response.json())
+    //     .then((data) => {
+            const photographers = data;
+            console.log(photographers);
+        // }); 
+                
+    return photographers
+}
 
-        // for each photographer we store in a variable the result of the factory function to which photographer is passed as an argument.
-        // we apply the getUserCardDOM method to it and store the result in a variable. 
-        // inserting the photographer card in the html photographer section
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+async function displayData(photographers) {
+    // pick up photographer html section
+    const photographersSection = document.querySelector(".photographer_section");
     
-    init();
+    // for each photographer we store in a variable the result of the factory function to which photographer is passed as an argument.
+    // we apply the getUserCardDOM method to it and store the result in a variable. 
+    // inserting the photographer card in the html photographer section
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
+
+// Penser à remplacer par les données récupérées dans le json
+// const photographers = [
+//     {
+//         "name": "Ma data test",
+//         "id": 1,
+//         "city": "Paris",
+//         "country": "France",
+//         "tagline": "Ceci est ma data test",
+//         "price": 400,
+//         "portrait": "account.png"
+//     },
+//     {
+//         "name": "Autre data test",
+//         "id": 2,
+//         "city": "Londres",
+//         "country": "UK",
+//         "tagline": "Ceci est ma data test 2",
+//         "price": 500,
+//         "portrait": "account.png"
+//     },
+// ]
+// // et bien retourner le tableau photographers seulement une fois
+// return ({
+//     photographers: [...photographers, ...photographers, ...photographers]})
