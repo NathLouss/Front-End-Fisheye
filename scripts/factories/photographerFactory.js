4d9e8dec9d312b4d457f1ab8a888c7a904463358
nathalie loussouarn <natel@hotmail.com>
2022-06-24 11:52:13 +0200

    add link on photographer card

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,6 +1,6 @@
 //pattern factory
 function photographerFactory(data) {
-    const { name, city, country, tagline, price, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/IDPhotos/${portrait}`;
     const place = `${city}, ${country}`;
     const sentence = `${tagline}`;
@@ -9,30 +9,35 @@ function photographerFactory(data) {
     // creation of html elements of the photographer card
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", `photographer/${id}.html`);
+        link.setAttribute("role", "navigation");
+        article.appendChild(link);
 
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait de ${name} - Page de ces réalisations`);
+        img.setAttribute("title", `Portrait de ${name}`);
+        link.appendChild(img);
 
-        const h2 = document.createElement( 'h2' );     
+        const h2 = document.createElement( 'h2' );    
         h2.textContent = name;
+        link.appendChild(h2);
 
         const position = document.createElement( 'p' );
         position.textContent = place;
         position.classList.add("position");
+        article.appendChild(position);
         
         const description = document.createElement( 'p' );
         description.classList.add("description");
         description.textContent = sentence;
+        article.appendChild(description);
         
         const rate = document.createElement( 'p' );
         rate.classList.add("rate");
         rate.textContent = cost;
-
-
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(position);
-        article.appendChild(description);
         article.appendChild(rate);
 
         return (article);
