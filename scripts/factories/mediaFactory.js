413f597eb167cd0424ba7e31ca5df3b80584b038
nathalie loussouarn <natel@hotmail.com>
2022-07-06 22:19:55 +0200

    function with image/video distinction in mediafactory

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,48 +1,53 @@
 function mediaFactory(dataMedias) {
-  const { photographerName, title, image, likes } = dataMedias;
+  const { photographerName, title, image, video, likes } = dataMedias;
   const picture = `assets/photographers/${photographerName}/${image}`;
-
+  
   function getMediaCardDOM() {
     const articlePortfolio = document.createElement( 'article' );
 
-    // if (image !== "undefined" && image !== null) {
-      articlePortfolio.classList.add("image");
-
+    if (image !== undefined && image !== null) {
       const img = document.createElement( 'img' );
       img.setAttribute("src", picture);
       img.setAttribute("alt", "");
       articlePortfolio.appendChild(img);
+    } else if (video !== undefined && video !== null) {
+      const capture = `assets/photographers/${photographerName}/${video.replace('mp4','png')}`;
+      const mp4 = document.createElement( 'video' );
+      mp4.setAttribute("poster", capture);
+      mp4.setAttribute("width", "100%");
+      mp4.setAttribute("height", "300px");
+      const src = document.createElement( 'source' );
+      src.setAttribute("src", video);
+      src.setAttribute("type", "video/mp4");
+      mp4.appendChild(src);
+      articlePortfolio.appendChild(mp4);
+    }
 
-      const divPortfolioText = document.createElement( 'div' );    
-      divPortfolioText.classList.add("article_text");
-      articlePortfolio.appendChild(divPortfolioText);
+    const divPortfolioText = document.createElement( 'div' );    
+    divPortfolioText.classList.add("article_text");
+    articlePortfolio.appendChild(divPortfolioText);
 
-      const titleImg = document.createElement( 'p' );    
-      titleImg.textContent = title;
-      divPortfolioText.appendChild(titleImg);
+    const titleImg = document.createElement( 'p' );    
+    titleImg.textContent = title;
+    divPortfolioText.appendChild(titleImg);
 
-      const divLike = document.createElement( 'div' );    
-      divLike.classList.add("article_like");
-      divPortfolioText.appendChild(divLike);
+    const divLike = document.createElement( 'div' );    
+    divLike.classList.add("article_like");
+    divPortfolioText.appendChild(divLike);
 
-      const likeNumber = document.createElement( 'p' );    
-      likeNumber.classList.add("likes");
-      likeNumber.textContent = likes;
-      divLike.appendChild(likeNumber);
+    const likeNumber = document.createElement( 'p' );    
+    likeNumber.classList.add("likes");
+    likeNumber.textContent = likes;
+    divLike.appendChild(likeNumber);
 
-      const icon = document.createElement( 'i' );
-      icon.setAttribute("class", "fas fa-heart");
-      divLike.appendChild(icon);
+    const icon = document.createElement( 'i' );
+    icon.setAttribute("class", "fas fa-heart");
+    divLike.appendChild(icon);
 
-      return (articlePortfolio)
-
-    // } else {
-    //   articlePortfolio.classList.add("video");
-    // }
-
+    return (articlePortfolio)
   }
 
-  return { title, image, likes, getMediaCardDOM }
+  return { title, image, video, likes, getMediaCardDOM }
 }
 
-// photographerId, title, image, vidéo, likes, date, price
+// photographerId, date, price
