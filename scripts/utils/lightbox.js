f71850822ed6af9152b0775e1fe782a1fa8e747e
nathalie loussouarn <natel@hotmail.com>
2022-10-13 22:09:22 +0200

    lightbox css, getSlideBtn function

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -5,28 +5,52 @@ import { mediaFactory } from '../factories/mediaFactory.js';
 // récupération éléments du DOM
 const lightbox = document.querySelector('.lightbox');
 const lightboxContainer = document.querySelector('.lightbox_container');
+const lightboxContent = document.querySelector('.lightbox_content');
 const closeBtn = document.querySelector('.svg_cross');
+const mediasSection = document.querySelector('.slides_list');
+let executed = false;
 
 // // lancement de la lightbox
 export function openLightboxModal(currentPosition, selectedMedias) {
   lightbox.style.display = 'block';
   lightboxContainer.style.display = 'block';
   displayMediasInLightbox(selectedMedias);
-  currentSlide(currentPosition);
+  // currentSlide(currentPosition);
+  getSlideBtnOnce();
   showSlides(slideIndex);
-  plusSlides(n)
+  // plusSlides(n)
 }
 
 // fermeture de la lightbox
 function closeLightbox(){
   lightbox.style.display = 'none';
   lightboxContainer.style.display = 'none';
+  mediasSection.innerHTML = '';
+}
+
+// exécution only once fonction getSlideBtn
+function getSlideBtnOnce() {
+  if (!executed) { 
+    getSlideBtn()
+  }
 }
 
 // création des boutons défilement 
-function getLightboxBtn() {
-    
-}
+function getSlideBtn() {
+  const previous = document.createElement('a');
+  previous.innerHTML = '<';
+  previous.classList.add('prev');
+  previous.setAttribute('aria-label', 'Contenu précédent');
+  lightboxContent.insertAdjacentElement('afterbegin', previous);
+
+  const next = document.createElement('a');
+  next.innerHTML = '>';
+  next.classList.add('next');
+  next.setAttribute('aria-label', 'Contenu suivant');
+  lightboxContent.insertAdjacentElement('beforeend' , next);
+
+  executed = true;
+};
 
 // eventlistener
 closeBtn.addEventListener('click', closeLightbox);
@@ -34,9 +58,7 @@ closeBtn.addEventListener('click', closeLightbox);
 
 // affichage des medias du photographe sélectionné dans la lightbox via la modalFactory
 function displayMediasInLightbox(selectedMedias) {
-  const mediasSection = document.querySelector('.slides_list');
-
-  selectedMedias.forEach((media) => {
+    selectedMedias.forEach((media) => {
     // media.photographerName = photographerName;
     // media.currentPosition = currentPosition;
     const mediaModel = mediaFactory(media);
@@ -50,16 +72,16 @@ function displayMediasInLightbox(selectedMedias) {
 let slideIndex = 1;
 // showSlides(slideIndex);
 
-function plusSlides(n) {
+function changeSlide(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function toSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
+    // let i;
     var slides = document.getElementsByClassName('slide');
     if (n > slides.length) {
         slideIndex = 1
@@ -67,7 +89,7 @@ function showSlides(n) {
     if (n < 1) {
         slideIndex = slides.length
     }
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
     slides[slideIndex-1].style.display = 'flex';
