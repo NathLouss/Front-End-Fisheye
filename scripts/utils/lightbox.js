766af76b18b04e9801178b8837bfec95d4dd8bde
nathalie loussouarn <natel@hotmail.com>
2022-11-15 14:58:53 +0100

    focus debugging when playing videos

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -7,6 +7,7 @@ const lightboxBg = document.querySelector(".lightbox_background");
 const lightboxContainer = document.querySelector(".lightbox_container");
 const closeBtn = document.querySelector(".svg_cross");
 const mediasSection = document.querySelector(".slides_list");
+let slides;
 let activeSlide;
 let executed = false;
 
@@ -70,7 +71,7 @@ function changeMedia(n) {
 
 // affichage des medias dans la Lightbox
 function showSlides(p) {
-  var slides = document.getElementsByClassName("slide");
+  slides = document.getElementsByClassName("slide");
   // si dde position sup au total des slides,
   // réinitialise slideindex pour revenir au dbt
   if (p > slides.length) {
@@ -86,11 +87,13 @@ function showSlides(p) {
     slides[i].style.display = "none";
     slides[i].classList.remove("active");
     slides[i].setAttribute("aria-hidden", "true");
+    slides[i].blur();
   }
   // affiche le slide selon index
   slides[slideIndex - 1].style.display = "flex";
   slides[slideIndex - 1].classList.add("active");
   slides[slideIndex - 1].setAttribute("aria-hidden", "false");
+  slides[slideIndex - 1].focus();
 }
 
 // fermeture de la lightbox
@@ -109,31 +112,22 @@ function lightboxAccessibility() {
     main.setAttribute("aria-hidden", "true");
     lightboxBg.setAttribute("aria-hidden", "false");
     lightboxContainer.setAttribute("aria-hidden", "false");
-    activeSlide = document.querySelector(".slide.active");
-    activeSlide.focus();
-    document.addEventListener("keydown", (e) => trapFocus(e));
   } else {
     body.classList.remove("no-scroll");
     main.setAttribute("aria-hidden", "false");
     lightboxBg.setAttribute("aria-hidden", "true");
     lightboxContainer.setAttribute("aria-hidden", "false");
-    document.removeEventListener("keydown", (e) => trapFocus(e));
   }
 }
 
 // garde le focus dans la modale
 function trapFocus(e) {
-  let isTabPressed = e.key === "Tab";
   const focusableElements = "button:not([disabled])";
   const firstFocusableElement =
     lightboxContainer.querySelectorAll(focusableElements)[0];
   const focusableContent =
     lightboxContainer.querySelectorAll(focusableElements);
   const lastFocusableElement = focusableContent[focusableContent.length - 1];
-
-  if (!isTabPressed) {
-    return;
-  }
 
   if (e.shiftKey) {
     if (document.activeElement === firstFocusableElement) {
@@ -148,45 +142,40 @@ function trapFocus(e) {
   }
 }
 
-// lecture video au clavier
-function playVideo() {
-  let slideActive = document.querySelector(".active");
-  let slideActiveClass = slideActive.firstChild.className;
-  if (
-    slideActiveClass == "slideVideo" ||
-    slideActiveClass == "slideVideo inProgress"
-  ) {
-    if (slideActiveClass == "slideVideo inProgress") {
-      slideActive.firstChild.pause();
-      slideActive.firstChild.classList.remove("inProgress");
-    } else {
-      slideActive.firstChild.play();
-      slideActive.firstChild.classList.add("inProgress");
-    }
-  }
-}
-
-// désactive effet touche Enter
-function cancelEnter(e) {
-  e.preventDefault;
-}
-
-//------------------------------------------------------------------------------------------
-// eventlistener
-closeBtn.addEventListener("click", closeLightbox);
-lightboxContainer.addEventListener("keydown", (e) => {
-  const eventKey = e.key;
+// navigation lightbox au clavier
+document.addEventListener("keydown", (e) => {
+  const eventKey = e.code;
   if (eventKey === "ArrowRight") {
     changeMedia(1);
   } else if (eventKey === "ArrowLeft") {
     changeMedia(-1);
   } else if (eventKey === "Escape") {
     closeLightbox();
+  } else if (eventKey === "Space") {
+    playVideo();
+  } else if (eventKey === "Tab") {
+    trapFocus(e);
   }
 });
-lightboxContainer.addEventListener("keydown", (e) => {
-  e.code == "Space" && playVideo();
-});
-lightboxContainer.addEventListener("keydown", (e) => {
-  e.code == "Enter" && cancelEnter(e);
-});
+
+// lecture video au clavier
+function playVideo() {
+  activeSlide = document.querySelector(".active");
+  let slideActiveClass = activeSlide.firstChild.className;
+  if (
+    slideActiveClass == "slideVideo" ||
+    slideActiveClass == "slideVideo inProgress"
+  ) {
+    if (slideActiveClass == "slideVideo inProgress") {
+      activeSlide.firstChild.pause();
+      activeSlide.firstChild.classList.remove("inProgress");
+    } else {
+      activeSlide.firstChild.play();
+      activeSlide.firstChild.classList.add("inProgress");
+    }
+  }
+}
+
+//------------------------------------------------------------------------------------------
+// eventlistener
+closeBtn.addEventListener("click", closeLightbox);
